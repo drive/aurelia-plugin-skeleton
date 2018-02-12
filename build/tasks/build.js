db6de92e031019fa6433ceee070e79a8e4f1331e
Dima V <11880540+dima-v@users.noreply.github.com>
2018-02-13 09:26:24 +1300

    chore(build): add typing and improve sourcemaps generation (#1)

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,3 +1,4 @@
+let path = require('path');
 let gulp = require('gulp');
 let replace = require('gulp-replace');
 let typescript = require('gulp-typescript');
@@ -10,99 +11,119 @@ const destCJS = paths.output + 'commonjs';
 const destSJS = paths.output + 'system';
 
 let transpile = (options, dest) => {
-    let project = typescript.createProject('tsconfig.json', options);
+  let project = typescript.createProject('tsconfig.json', options);
 
-    let tsResult = gulp.src(paths.source)
-        .pipe(sourcemaps.init())
-        .pipe(project())
+  let tsResult = gulp.src(paths.source)
+    .pipe(sourcemaps.init())
+    .pipe(project())
 
-    return tsResult.js
-        .pipe(sourcemaps.write(''))
-        .pipe(gulp.dest(dest));
+  let js = tsResult.js
+    .pipe(sourcemaps.mapSources(function (sourcePath, file) {
+      let relativeLocation = path.join(path.relative(path.join(dest, path.dirname(file.relative)), '.'), 'src/');
+      let relativeLocationToFile = path.join(relativeLocation, sourcePath);
+      return relativeLocationToFile;
+    }))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest(dest));
+
+  return js;
 }
 
+let typings = () => {
+  let project = typescript.createProject('tsconfig.json');
+
+  let tsResult = gulp.src(paths.source)
+    .pipe(project())
+
+  let dts = tsResult.dts
+    .pipe(gulp.dest(paths.output + '/typings'));
+
+  return dts;
+}
+
+
 let html = () => {
-    return gulp.src(paths.html)
-        .pipe(gulp.dest(destEs2015))
-        .pipe(gulp.dest(destAMD))
-        .pipe(gulp.dest(destCJS))
-        .pipe(gulp.dest(destSJS));
+  return gulp.src(paths.html)
+    .pipe(gulp.dest(destEs2015))
+    .pipe(gulp.dest(destAMD))
+    .pipe(gulp.dest(destCJS))
+    .pipe(gulp.dest(destSJS));
 }
 
 let css = () => {
-    return gulp.src(paths.css)
-        .pipe(gulp.dest(destEs2015))
-        .pipe(gulp.dest(destAMD))
-        .pipe(gulp.dest(destCJS))
-        .pipe(gulp.dest(destSJS));
+  return gulp.src(paths.css)
+    .pipe(gulp.dest(destEs2015))
+    .pipe(gulp.dest(destAMD))
+    .pipe(gulp.dest(destCJS))
+    .pipe(gulp.dest(destSJS));
 }
 
 let es2015 = () => transpile({
-    module: 'es2015'
+  module: 'es2015'
 }, destEs2015);
 
 let amd = () => transpile({
-    module: 'amd'
+  module: 'amd'
 }, destAMD);
 
 let commonjs = () => transpile({
-    module: 'commonjs'
+  module: 'commonjs'
 }, destCJS);
 
 let system = () => transpile({
-    module: 'system'
+  module: 'system'
 }, destSJS);
 
 let timestamp = () => {
-    return gulp.src(paths.timestamp)
-        .pipe(replace('timestamp', Date.now()))
-        .pipe(gulp.dest(destEs2015))
-        .pipe(gulp.dest(destAMD))
-        .pipe(gulp.dest(destCJS))
-        .pipe(gulp.dest(destSJS))
+  return gulp.src(paths.timestamp)
+    .pipe(replace('timestamp', Date.now()))
+    .pipe(gulp.dest(destEs2015))
+    .pipe(gulp.dest(destAMD))
+    .pipe(gulp.dest(destCJS))
+    .pipe(gulp.dest(destSJS))
 }
 
 let build = gulp.series(
-    gulp.parallel(html, css, es2015, amd, commonjs, system),
-    timestamp
+  gulp.parallel(html, css, typings, es2015, amd, commonjs, system),
+  timestamp
 );
 
 let watchHtml = (done) => {
-    gulp.watch(
-        paths.html,
-        gulp.series(html, timestamp)
-    );
-    done();
+  gulp.watch(
+    paths.html,
+    gulp.series(html, timestamp)
+  );
+  done();
 }
 
 let watchCss = (done) => {
-    gulp.watch(
-        paths.css,
-        gulp.series(css, timestamp)
-    );
-    done();
+  gulp.watch(
+    paths.css,
+    gulp.series(css, timestamp)
+  );
+  done();
 }
 
 let watchSource = (done) => {
-    gulp.watch(
-        paths.source,
-        gulp.series(
-            gulp.parallel(es2015, amd, commonjs, system),
-            timestamp)
-    );
-    done();
+  gulp.watch(
+    paths.source,
+    gulp.series(
+      gulp.parallel(es2015, amd, commonjs, system),
+      timestamp)
+  );
+  done();
 }
 
 let watch = gulp.series(
-    build,
-    gulp.parallel(
-        watchHtml,
-        watchCss,
-        watchSource
-    )
+  build,
+  gulp.parallel(
+    watchHtml,
+    watchCss,
+    watchSource
+  )
 );
 
 module.exports = {
-    run: build,
-    watch: watch
+  run: build,
+  watch: watch
 };
